99f0753aae11f3e28b31c4a199b5d4f39f79d40a
이상남 <lsn5963@naver.com>
2022-09-27 22:54:00 +0900

    refactor: sql 로그 남기기

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -15,6 +17,7 @@ public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
     public void testMember() throws Exception{
         Member member = new Member();
         member.setUsername("memberA");
