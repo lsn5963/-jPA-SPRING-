ec13715a19f09e906a7bf769bc2148302092273e
이상남 <lsn5963@naver.com>
2022-10-08 00:37:17 +0900

    refactor: 연관관계 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Entity
 @Getter
+@Setter
 public class Category {
     @Id @GeneratedValue
     @Column(name = "category_id")
@@ -22,10 +24,16 @@ public class Category {
         inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_od")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
