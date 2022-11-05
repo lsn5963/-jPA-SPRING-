6129c7cf36a62c079f5e1e975ce9ea406aa45c32
이상남 <lsn5963@naver.com>
2022-11-05 23:48:27 +0900

    feat: 주문엔티티 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domain;
 
 import jpabook.jpashop.Member;
 import lombok.Getter;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter
+@Setter
 public class Order {
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -46,5 +49,39 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+    //== 생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem: orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
 
+    //== 비즈니스 로직==//
+    //주문취소
+    public void cancel(){
+        if (delivery.getStatus() == DeliverySatus.COMP){    //배송완료
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    //전체 주문 가격 조회
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -24,4 +24,24 @@ public class OrderItem {
 
     private int orderPrice; //주문 당시 가격
     private int count;  //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {  //재고수량을 다시 늘려준다
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice()* getCount();
+    }
 }
